c468ead5b48ce6312f747f3a5005acf0e8d85e70
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-27 09:05:26 -0400

    feat: setup initial nodes and edges

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,80 @@
-import  ReactFlow, {Background, Controls} from "reactflow"
-import 'reactflow/dist/style.css'
+import ReactFlow, {
+  addEdge,
+  Background,
+  Connection,
+  ConnectionMode,
+  Controls,
+  Node,
+  useEdgesState,
+  useNodesState,
+} from "reactflow";
+import { zinc } from "tailwindcss/colors";
+import "reactflow/dist/style.css";
+import { Square } from "./components/nodes/Square";
+import { useCallback } from "react";
+import DefaultEdge from "./components/edges/DefaultEdge";
+
+const NODE_TYPES = {
+  square: Square,
+};
+
+const EDGE_TYPES = {
+  default: DefaultEdge
+}
+
+const INITIAL_NODES = [
+  {
+    id: crypto.randomUUID(),
+    type: "square",
+    position: {
+      x: 200,
+      y: 400,
+    },
+    data: {},
+  },
+  {
+    id: crypto.randomUUID(),
+    type: "square",
+    position: {
+      x: 1000,
+      y: 400,
+    },
+    data: {},
+  },
+] satisfies Node[];
 
 function App() {
 
+const [edges, setEdges, onEdgesChange] = useEdgesState([])
+const [nodes, setNodes, onNodesChange] = useNodesState(INITIAL_NODES)
+
+
+
+const onConnect = useCallback((connection : Connection) => {
+
+return setEdges(edges => addEdge(connection, edges))}, [])
+
+
   return (
-    <div style={{width: '100vw', height: '100vh'}}>
-   <ReactFlow>
-    <Background
-    gap={12}
-    size={2}
-    color='#ddd'
-    />
-   </ReactFlow>
-   </div>
-  )
+    <div className="w-screen h-screen">
+      <ReactFlow
+        nodeTypes={NODE_TYPES}
+        edgeTypes={EDGE_TYPES}
+        nodes={nodes}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onNodesChange={onNodesChange}
+        onConnect={onConnect}
+        connectionMode={ConnectionMode.Loose}
+        defaultEdgeOptions={{
+          type: 'default'
+        }}
+      >
+        <Background gap={12} size={2} color={zinc[200]} />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
 }
 
-export default App
+export default App;
