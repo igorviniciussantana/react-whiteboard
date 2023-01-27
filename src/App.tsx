19e951cc3a72b607faef9320a3e87bad8c12d743
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-27 08:10:23 -0400

    feat: add Background styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ import 'reactflow/dist/style.css'
 function App() {
 
   return (
-   <h1>Hello World</h1>
+    <div style={{width: '100vw', height: '100vh'}}>
+   <ReactFlow>
+    <Background
+    gap={12}
+    size={2}
+    color='#ddd'
+    />
+   </ReactFlow>
+   </div>
   )
 }
 
