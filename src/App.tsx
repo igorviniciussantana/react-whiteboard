84747a28046d01d0f06b249366b760e610979324
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-02-02 07:30:37 -0400

    feat: add new node function

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import ReactFlow, {
   useEdgesState,
   useNodesState,
 } from "reactflow";
+import * as Toolbar from '@radix-ui/react-toolbar'
 import { zinc } from "tailwindcss/colors";
 import "reactflow/dist/style.css";
 import { Square } from "./components/nodes/Square";
@@ -55,6 +56,21 @@ const onConnect = useCallback((connection : Connection) => {
 return setEdges(edges => addEdge(connection, edges))}, [])
 
 
+
+function addSquareNode(){
+  setNodes(nodes => [
+    ...nodes,
+    {
+      id: crypto.randomUUID(),
+      type: "square",
+      position: {
+        x: 750,
+        y: 350,
+      },
+      data: {},
+    }
+  ])
+}
   return (
     <div className="w-screen h-screen">
       <ReactFlow
@@ -73,6 +89,11 @@ return setEdges(edges => addEdge(connection, edges))}, [])
         <Background gap={12} size={2} color={zinc[200]} />
         <Controls />
       </ReactFlow>
+      <Toolbar.Root className="fixed bottom-2 left-1/2 -translate-x-1/2 bg-white rounded-2xl shadow-lg border border-zinc-300 px-8 h-20 w-96 overflow-hidden">
+        <Toolbar.Button onClick={addSquareNode} className="w-32 h-32 bg-violet-500 mt-6 rounded transition-transform hover:-translate-y-2"/>
+
+
+      </Toolbar.Root>
     </div>
   );
 }
